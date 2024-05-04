0ab104e6a9b26df60ce1103ecd3de30bf63fbbc3
Maria Mikhaylova <mihailova_ms@gkl-kemerovo.ru>
2024-05-04 19:16:41 +0300

    aa

--- index.js
+++ index.js
@@ -2,22 +2,12 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import sequelize from "./db.js";
-import models from "./src/models/models.js";
-import {
-  registerValid,
-  loginValid,
-  articleValid,
-  announcementValid,
-} from "./src/validations/valids.js";
+import { registerValid, loginValid } from "./src/validations/valids.js";
 import checkAuth from "./src/utils/checkAuth.js";
 
 import {
   userController,
-  articleController,
-  announcementController,
   defaultValueController,
-  chatController,
-  messageController,
 } from "./src/controllers/index.js";
 import {
   initializeStatusesArticle,
@@ -26,7 +16,11 @@ import {
   initializeCity,
 } from "./src/initialize/index.js";
 
-const { User } = models;
+import ArticleRoter from "./src/routes/ArticleRouter.js";
+import AnnouncementRoter from "./src/routes/AnnouncementRouter.js";
+import ChatRouter from "./src/routes/ChatRouter.js";
+import MessageRouter from "./src/routes/MessageRouter.js";
+import AuthRouter from "./src/routes/AuthRouter.js";
 
 dotenv.config();
 
@@ -36,40 +30,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  const users = await User.findAll();
-  res.json(users);
-});
+app.use("/articles", checkAuth, ArticleRoter);
+app.use("/announcement", checkAuth, AnnouncementRoter);
+app.use("/chat", ChatRouter);
+app.use("/message", MessageRouter);
+app.use("/auth", AuthRouter);
 
-app.get("/auth/me", checkAuth, userController.getMe);
-app.post("/login", loginValid, userController.login);
-app.post("/register", registerValid, userController.register);
+// Переделать на роуты
+// app.get("/auth/me/:id?", checkAuth, userController.getMe);
+// app.post("/login", loginValid, userController.login);
+// app.post("/register", registerValid, userController.register);
 
-// ПРОВЕРИТЬ ВЕЗДЕ НА АУНТИФИКАЦИЮ
-app.post("/articles", checkAuth, articleValid, articleController.create);
-app.get("/articles", checkAuth, articleController.getAll);
-app.get("/articles/:id", checkAuth, articleController.getOne);
-app.delete("/articles/:id", checkAuth, articleController.remove);
-app.patch("/articles/:id", checkAuth, articleController.update);
-
-app.post(
-  "/announcement",
-  checkAuth,
-  announcementValid,
-  announcementController.create
+app.get("/users/:id", userController.getUser);
+app.get("/users", checkAuth, userController.getAllUsers);
+app.post("/users/:userId/friends/:friendId", userController.createSubscription);
+app.delete(
+  "/users/:userId/friends/:friendId",
+  userController.deleteSubscription
 );
-app.get("/announcement", checkAuth, announcementController.getAllByCity);
-app.get("/announcement/:id", checkAuth, announcementController.getOne);
-app.delete("/announcement/:id", checkAuth, announcementController.remove);
-app.patch("/announcement/:id", checkAuth, announcementController.update);
+app.get("/users/:userId/friends", userController.getUserSubscription);
 
 app.get("/cities", defaultValueController.getAllCities);
-
-app.post("/chats", chatController.create);
-app.get("/chats", chatController.getOneChat);
-app.get("/chats/all", chatController.getAllChats);
-
-app.post("/message", messageController.create);
+app.get("/types", defaultValueController.getTypes);
 
 const start = async () => {
   try {
@@ -89,3 +71,21 @@ const start = async () => {
 };
 
 start();
+
+// ПРОВЕРИТЬ ВЕЗДЕ НА АУНТИФИКАЦИЮ
+// app.post("/articles", checkAuth, articleValid, articleController.create);
+// app.get("/articles", checkAuth, articleController.getAll);
+// app.get("/articles/:id", checkAuth, articleController.getOne);
+// app.delete("/articles/:id", checkAuth, articleController.remove);
+// app.patch("/articles/:id", checkAuth, articleController.update);
+
+// app.post(
+//   "/announcement",
+//   checkAuth,
+//   announcementValid,
+//   announcementController.create
+// );
+// app.get("/announcement", checkAuth, announcementController.getAllByCity);
+// app.get("/announcement/:id", checkAuth, announcementController.getOne);
+// app.delete("/announcement/:id", checkAuth, announcementController.remove);
+// app.patch("/announcement/:id", checkAuth, announcementController.update);
