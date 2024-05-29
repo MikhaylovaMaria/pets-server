059863038fba88f4c1da7d8870ae9073f1aa0a40
Maria Mikhaylova <mihailova_ms@gkl-kemerovo.ru>
2024-05-29 12:18:20 +0300

    test 3

--- db.js
+++ db.js
@@ -3,12 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-
 const sequelize = new Sequelize(
   process.env.DB_NAME, // название БД
   process.env.DB_USER, // пользователь
@@ -19,13 +13,5 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT,
   }
 );
-
-sequelize.authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
 
 export default sequelize;
