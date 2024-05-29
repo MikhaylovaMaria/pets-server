5cf5cf5689b95635707455ab70351d2a2295adcb
Maria Mikhaylova <mihailova_ms@gkl-kemerovo.ru>
2024-05-29 13:01:30 +0300

    test 4

--- db.js
+++ db.js
@@ -3,15 +3,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME, // название БД
-  process.env.DB_USER, // пользователь
-  process.env.DB_PASSWORD, //Пароль
-  {
-    dialect: "postgres",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME, // название БД
+//   process.env.DB_USER, // пользователь
+//   process.env.DB_PASSWORD, //Пароль
+//   {
+//     dialect: "postgres",
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//   }
+// );
+
+const sequelize = new Sequelize(process.env.DB_URL)
 
 export default sequelize;
